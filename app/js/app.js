964dfac90509887ca75de5b2bc4d5f0d3c87b225
Luis Perichon <luisperichon@gmail.com>
2014-07-17 16:57:12 -0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,93 @@
+(function(){
+	'use strict';
+	var app = angular.module('myApp', ['onsen.directives', 'google-maps']);
+
+	app.factory('Deals', function($http) {
+		var dealId;
+	    var Deals = {
+	        endpoint: 'http://localhost:3333/api',
+
+	        setDealId: function(id) {
+	        	dealId = id;
+	        },
+
+	        getDealId: function() {
+	        	return dealId;
+	        },
+
+	        getDeals: function() {
+	            return this.makeCall(this.endpoint + '/deals.json?callback=JSON_CALLBACK');
+	        },
+	        
+	        getDeal: function(id) {
+	            return this.makeCall(this.endpoint + '/deals/'+id+'.json?callback=JSON_CALLBACK');
+	        },
+
+	        makeCall: function(url){
+	            return $http.jsonp(url).then(function(response){
+	                return response.data;
+	            });            
+	        }
+	    };
+	    return Deals;
+	});
+	
+	app.controller("mapController", function($scope, $location, Deals) {
+	    $scope.map = {
+		    center: {
+		        latitude: 45,
+		        longitude: -73
+		    },
+		    zoom: 8
+		};
+
+		// Center map using geolocation
+		var onSuccess = function(position) {
+		    $scope.map.center = {
+		        latitude: position.coords.latitude,
+		        longitude: position.coords.longitude
+		    };
+		    $scope.$apply();
+		}
+		var onError = function(error) {
+		    console.log('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+		}
+		
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+		// Open marker page on click
+		$scope.markersEvents = {
+		    click: function (gMarker, eventName, model) {
+		      var element = document.querySelector( ".navigator-container");
+        	  var scope = angular.element( element ).scope();
+        	  // Store the model id on the service for use on the markerController
+        	  Deals.setDealId(model.id);
+		      scope.pushPage('marker.html');
+		    }
+		};
+
+		$scope.getDeals = function() {
+			Deals.getDeals().then(function(data) {
+				$scope.markers = data;
+			});
+		};
+
+
+		// call service
+		$scope.getDeals();
+	});
+
+	app.controller("markerController", function($scope, $location, Deals) {
+	    
+		$scope.getDeal = function() {
+			// Use the id stored on the service (on the marker click event) to load the full data for the Deal.
+			Deals.getDeal(Deals.getDealId()).then(function(data) {
+				$scope.deal = data;
+			});
+		};
+
+
+		// call service
+		$scope.getDeal();
+	});
+})();
